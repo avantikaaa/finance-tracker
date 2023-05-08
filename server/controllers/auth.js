4db68a127a9683450ad48caed23369640613c7f7
avantika <avantika.mudumbai@gmail.com>
2023-05-09 00:22:30 +0530

    sever full working code

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const secret = process.env.SECRET_KEY;
+const salt = bcrypt.genSaltSync(10);
 
 module.exports = {
 	login: async(req, res) => {
@@ -18,7 +19,7 @@ module.exports = {
 				});
 			});
 			} else {
-			res.json("Invalid Credentials");
+			res.status(401).json({});
 			}
 		} catch (e) {
 			console.log(e);
@@ -28,33 +29,36 @@ module.exports = {
 
 	logout: async(req, res) => {
 		res.cookie("token", "").json("ok");
+	},
+
+	register: async (req, res) => {
+		const { username, password, name, limit } = req.body;
+		const all_usernames = await User.aggregate([
+			{
+				$match:{
+					username: username
+				}
+			}
+		]);
+		console.log(all_usernames);
+		if(all_usernames.length == 0){
+			try {
+				const userDoc = await User.create({
+				username,
+				password: bcrypt.hashSync(password, salt),
+				name,
+				// college,
+				// yearOfStudy: yearOfStudy,
+				limit: limit,
+				});
+				res.json({ requestData: { username, password } });
+			} catch (e) {
+				console.log(e);
+				res.status(404).json(e);
+			}
+		}
+		else{
+			res.status(409).json({});
+		}
 	}
 }
-
-
-// async function login(res, req){
-// 	const { username, password } = req.body;
-// 	try {
-// 		const UserDoc = await User.findOne({ username });
-// 		const passOk = bcrypt.compareSync(password, UserDoc.password);
-// 		if (passOk) {
-// 		jwt.sign({ username, id: UserDoc._id }, secret, {}, (err, token) => {
-// 			if (err) throw err;
-// 			res.cookie("token", token).json({
-// 			id: UserDoc._id,
-// 			username,
-// 			});
-// 		});
-// 		} else {
-// 		alert("Invalid");
-// 		res.json("Invalid Credentials");
-// 		}
-// 	} catch (e) {
-// 		console.log(e);
-// 		res.status(404).json(e);
-// 	}
-// }
-
-// let logout =  async(req, res) => {
-// 	res.cookie("token", "").json("ok");
-// }
