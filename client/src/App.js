48bde01b25bf39693cfddfbb982dd843d63dc14a
Neelabh P <neel.pullikol@gmail.com>
2023-05-08 23:39:20 +0530

    client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,42 @@
 import "./App.css";
-import Post from "./components/DetailsCard";
-import Header from "./components/Header";
-import AddLending from "./components/AddLending";
-import AddIncome from "./components/AddIncome";
-import AddExpense from "./components/AddExpense";
 import { Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./components/pages/LoginPage";
 import Layout from "./Layout";
-import HomePage from "./pages/HomePage";
-import RegisterPage from "./pages/RegisterPage";
+import RegisterPage from "./components/pages/RegisterPage";
 import { UserContextProvider } from "./contexts/UserContext";
-import IntroPage from "./pages/IntroPage";
+import AddTransaction from "./components/pages/AddTransaction";
+import IntroPage from "./components/pages/IntroPage";
+import Peer from "./components/pages/Peer"
+import Transaction from "./components/pages/Transaction";
+import Friends from "./components/pages/Friends";
+import ReminderScreen from "./components/pages/ReminderScreen"
+import Logoutpop from "./components/pages/Logoutpop"
+import Settings from "./components/pages/Settings";
+import ExpensesPage from "./components/pages/ExpensesPage";
+import SavingsCalculator from "./components/pages/SavingsCalculator"
+import Dashboard from "./components/Dashboard/Dashboard"
+import LeftNav from "./components/Layout/LeftNav";
+
 function App() {
   return (
     <UserContextProvider>
       <Routes>
+        <Route path = "/intro" element = {<IntroPage/>}></Route>
+        <Route index element = {<IntroPage/>}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+
         <Route path="/" element={<Layout />}>
-          <Route path= "/homepage" element={<HomePage />}></Route>
-          <Route path="/login" element={<LoginPage />}></Route>
-          <Route path="/register" element={<RegisterPage />}></Route>
-          <Route path ="/addLending" element = {<AddLending />}></Route> 
-          <Route path ="/addExpense" element = {<AddExpense/>}></Route>
-          <Route path = "/addIncome" element = {<AddIncome />}></Route>
-          <Route index element = {<IntroPage />}></Route>
-          <Route path = "/intro" element = {<IntroPage/>}></Route>
+          <Route index element={<Dashboard />}></Route>
+          <Route path = "/homepage" element={<Dashboard />}></Route>
+          <Route path ="/create" element = {<ExpensesPage/>}></Route>   
+          <Route path = "/peer" element = {<Peer/>}></Route>
+          <Route path = "/transaction" element = {<Transaction/>}></Route>
+          <Route path = "/friends" element = {<Friends/>}></Route>
+          <Route path = "/reminder" element = {<ReminderScreen/>}></Route>
+          <Route path = "/logout" element = {<Logoutpop/>}></Route>
+          <Route path = "/settings" element = {<Settings/>}></Route>
+          <Route path = "/savings" element = {<SavingsCalculator/>}></Route>
           </Route>
       </Routes>
     </UserContextProvider>
