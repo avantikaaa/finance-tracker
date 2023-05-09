1ce02d9a1b881d72c1119170bcca660c9ddba643
avantika <avantika.mudumbai@gmail.com>
2023-05-09 20:20:59 +0530

    docker files

--- server/index.js
+++ server/index.js
@@ -42,8 +42,8 @@ const logger = winston.createLogger({
 module.exports = logger;
 
 mongoose.set("strictQuery", true);
-mongoose.connect('mongodb://mongodb:27017/mydb', { useNewUrlParser: true, useUnifiedTopology: true })
-// mongoose.connect('mongodb://localhost:27017/mydb', { useNewUrlParser: true, useUnifiedTopology: true })
+// mongoose.connect('mongodb://mongodb:27017/mydb', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/mydb', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
